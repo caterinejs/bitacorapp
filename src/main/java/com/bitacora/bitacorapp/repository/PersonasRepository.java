43135a35cf573c931ade095892aaa9004ada6c68
juannobl <jpnorena@sura.com.co>
2023-02-15 16:43:50 -0500

    se agregan librerias base de datos

--- src/main/java/com/bitacora/bitacorapp/repository/PersonasRepository.java
+++ src/main/java/com/bitacora/bitacorapp/repository/PersonasRepository.java
@@ -1,11 +1,13 @@
 package com.bitacora.bitacorapp.repository;
 
 import com.bitacora.bitacorapp.domain.PersonasDomain;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonasRepository extends JpaRepository<PersonasDomain, String> {
+public interface PersonasRepository extends CrudRepository<PersonasDomain, String> {
 
-    @Query("{'name':?0}")
+    @Query("SELECT p FROM Person u WHERE u.name = ?1")
     public PersonasDomain findByName(String name);
 }
